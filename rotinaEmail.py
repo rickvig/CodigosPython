1d4f90fff3b7b61961e6c6834fa0061bac2bcfc4
Henrique Vignando <rickuev@gmail.com>
2019-10-07 18:28:48 -0300

    opa

--- rotinaEmail.py
+++ rotinaEmail.py
@@ -16,6 +16,7 @@ most_recent = inbox_item_list[-1]
 oldest = inbox_item_list[0]
 
 for item in inbox_item_list:
+    
     result2, email_data = mail.uid('fetch', item, '(RFC822)')
     raw_email = email_data[0][1].decode("utf-8")
     email_message = email.message_from_string(raw_email)
@@ -23,25 +24,35 @@ for item in inbox_item_list:
     from_ = email_message['From']
     subject_ = email_message['Subject']
     date_ = email_message['date']
+    
     counter = 1
     for part in email_message.walk():
+        
         if part.get_content_maintype() == "multipart":
             continue
+        
         filename = part.get_filename()
         content_type = part.get_content_type()
+        
         if not filename:
             ext = mimetypes.guess_extension(content_type)
+            
             if not ext:
                 ext = '.bin'
+            
             if 'text' in content_type:
                 ext = '.txt'
             elif 'html' in content_type:
                 ext = '.html'
+            
             filename = 'msg-part-%08d%s' %(counter, ext)
+        
         counter += 1
-    save_path = os.path.join(dirOpa, subject_)
-    #if not os.path.exists(save_path):
-     #   os.makedirs(save_path)
-    with open(os.path.join(save_path, filename), 'wb') as fp:
-        fp.write(part.get_payload(decode=True))
+        save_path = os.path.join(dirOpa, subject_)
+        
+        if not os.path.exists(save_path):
+          os.makedirs(save_path)
+        
+        with open(os.path.join(save_path, filename), 'wb') as fp:
+            fp.write(part.get_payload(decode=True))
 
